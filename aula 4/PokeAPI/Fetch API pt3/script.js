26ae4312feac9776cc9f78b24818a64c49bb4292
Tauã Felipe <127506090+felipetaua@users.noreply.github.com>
2024-06-27 08:35:28 -0300

    Add files via upload

---
+++ aula 4/PokeAPI/Fetch API pt3/script.js
@@ -0,0 +1,31 @@
+//Buscando elementos doobjeto da requisição Fetch
+
+//Criando a função assincrona
+async function fetchPokemon(){
+    try {
+        const pokemonID = document.getElementById('pokemonID').value
+        const pokemonNome = document.getElementById('pokemonNome')
+        const pokemonType = document.getElementById('pokemonType')
+
+        const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`)
+
+        if(!resp.ok){
+            throw new Error('Pokemon não encontrado');
+        } else {
+            const data = await resp.json();
+            console.log(data);
+            const imgElemento = document.getElementById('pokemonSprite');
+            const pokemonSprite = data.sprites.front_default;
+            const pokemonNomeData = data.name;
+            //metodos com array
+            const pokemonTipoData = data.types[0].type.name;
+
+            imgElemento.src = pokemonSprite
+            imgElemento.style.display = 'block';
+            pokemonNome.innerHTML = pokemonNomeData;
+            pokemonType.innerHTML = pokemonTipoData;
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
